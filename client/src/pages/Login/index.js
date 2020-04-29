f57da0c414b67c7743da72c97a638b7a3812d7b5
John Ochs <john.w.ochs0@gmail.com>
2020-04-28 21:21:04 -0500

    completed encrypting passwords to db. bcrypt

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -4,6 +4,7 @@ import api from "../../utils/api";
 
 function Login() {
     const [state, change] = useState({ drawer: ["closed", "z-depth-3 open"] });
+    const [passwordInvalid, classInvalid] = useState("");
     const [r, t] = useState("");
     const [o, p] = useState("");
     const [j, k] = useState("");
@@ -21,15 +22,24 @@ function Login() {
 
     const registerName = qq => k(qq.target.value);
     const registerEmail = qw => z(qw.target.value);
-    const registerPassword = qe => qe[1] ? qt(qe[0].target.value) : c(qe[0].target.value);
+    const registerPassword = qe => {
+        qe[1] ? qt(qe[0].target.value) : c(qe[0].target.value);
+    }
     const registerSubmit = qu => {
         qu.preventDefault();
-        console.log(j + l + qr + x);
-        api.newUser({
-            name: j,
-            email: l,
-            password: qr
-        })
+        const qo = ["invalid", ""];
+        if (qt.length < 8 || c.length < 8) classInvalid(qo[0]);
+        else if (x !== qr) classInvalid(qo[0]);
+        else {
+            classInvalid(qo[1]);
+            if (j !== "" && l !== "" && qr !== "" && x === qr) {
+                api.newUser({
+                    name: j,
+                    email: l,
+                    password: qr
+                });
+            }
+        }
     }
 
     return (
@@ -40,10 +50,12 @@ function Login() {
                         <h3>Log in</h3>
                         <br /><br /><br />
                         <form>
-                            <label htmlFor="fname">email</label>
-                            <input type="text" id="fname" name="fname" onChange={y => setLoginEmail(y)} />
+                            <label for="email_inline">Email</label>
+                            <input id="email_inline" type="email" class="validate" onChange={y => setLoginEmail(y)} />
+                            <span class="helper-text" data-error="Please enter a valid email" data-success="right"></span>
+                            <br />
                             <label htmlFor="passw1">password</label>
-                            <input type="text" id="passw1" name="passw1" onChange={a => setLoginPassword(a)} />
+                            <input type="password" id="passw1" name="passw1" onChange={a => setLoginPassword(a)} />
                             <br /><br /><br /><br />
                             <button className="btn block green waves-effect waves-yellow" type="submit" name="action" onClick={d => {
                                 blockLoginSubmit(d);
@@ -55,14 +67,17 @@ function Login() {
                         <form encType="multipart/form-data">
                             <label htmlFor="name">full name</label>
                             <input type="text" id="name" name="name" onChange={v => registerName(v)} />
-                            <label htmlFor="email2">email</label>
-                            <input type="text" id="email2" name="email2" onChange={b => registerEmail(b)} />
+                            <label for="email_inline2">Email</label>
+                            <input id="email_inline2" type="email" class="validate" onChange={b => registerEmail(b)} />
+                            <span class="helper-text" data-error="Please enter a valid email" data-success="Email valid"></span>
+                            <br />
                             <label htmlFor="passw2">password</label>
-                            <input type="text" id="passw2" name="passw2" onChange={n => registerPassword([n, 0])} />
+                            <input type="password" id="passw2" name="passw2" class={passwordInvalid} onChange={n => registerPassword([n, 0])} />
                             <label htmlFor="passwconfirm2">confirm password</label>
-                            <input type="text" id="passwconfirm2" name="passwconfirm2" onChange={m => registerPassword([m, 1])} />
+                            <input type="password" id="passwconfirm2" name="passwconfirm2" class={passwordInvalid} onChange={m => registerPassword([m, 1])} />
+                            <span class="helper-text" data-error="Passwords must match and minimum 8 characters in length" data-success=""></span>
                             <br /><br />
-                            <button className="btn block green waves-effect waves-light" type="submit" name="action" onClick={qy => {
+                            <button className={"btn block green waves-effect waves-light"} type="submit" name="action" onClick={qy => {
                                 registerSubmit(qy);
                             }}>Submit<i className="material-icons right">send</i></button>
                         </form>
@@ -71,13 +86,15 @@ function Login() {
                         <h4 className="logTitle">Register</h4>
                         <form encType="multipart/form-data">
                             <label htmlFor="name">full name</label>
-                            <input type="text" id="name" name="name" />
-                            <label htmlFor="email2">email</label>
-                            <input type="text" id="email2" name="email2" />
+                            <input type="text" id="name" name="name" onChange={v => registerName(v)} />
+                            <label for="email_inline3">Email</label>
+                            <input id="email_inline3" type="email" class="validate" onChange={b => registerEmail(b)} />
+                            <span class="helper-text" data-error="Please enter a valid email" data-success="Email valid"></span>
+                            <br />
                             <label htmlFor="passw4">password</label>
-                            <input type="text" id="passw4" name="passw4" />
+                            <input type="password" id="passw4" name="passw4" class={"validate " + passwordInvalid} onChange={n => registerPassword([n, 0])} />
                             <label htmlFor="passwconfirm">confirm password</label>
-                            <input type="text" id="passwconfirm" name="passwconfirm" />
+                            <input type="password" id="passwconfirm" name="passwconfirm" class={"validate " + passwordInvalid} onChange={m => registerPassword([m, 1])} />
                             <button className="btn block green waves-effect waves-light" type="submit" name="action" >
                                 Submit<i className="material-icons right">send</i>
                             </button>
@@ -86,10 +103,12 @@ function Login() {
                     <div id="loginDrawer" className={state.drawer[1] + "1 drawer"} onClick={openDrawer.bind(this, 1)}>
                         <h4 className="logTitle">Log in</h4>
                         <form>
-                            <label htmlFor="fname">email</label>
-                            <input type="text" id="fname" name="fname" onChange={f => setLoginEmail(f)} />
+                            <label for="email_inline4">Email</label>
+                            <input id="email_inline4" type="email" class="validate" onChange={f => setLoginEmail(f)} />
+                            <span class="helper-text" data-error="Please enter a valid email" data-success="Email valid"></span>
+                            <br />
                             <label htmlFor="passw6">password</label>
-                            <input type="text" id="passw6" name="passw6" onChange={g => setLoginPassword(g)} />
+                            <input type="password" id="passw6" name="passw6" onChange={g => setLoginPassword(g)} />
                             <button className="btn block green waves-effect waves-yellow" type="submit" name="action" onClick={h => {
                                 blockLoginSubmit(h);
                             }}>Log in</button>
--- controller/userController.js
+++ controller/userController.js
@@ -2,21 +2,6 @@ const db = require("../models");
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-    // findAll: function (req, res) {
-    //     db.Users.find(req.query)//.sort({ date: -1 })
-    //         .then(img => res.json(img))
-    //         .catch(err => res.status(422).json(err));
-    // },
-
-
-    // console.log(req.body)
-    // bcrypt.genSalt(10, function (err, salt) {
-    //     bcrypt.hash(req.password, salt, function (err, hash) {
-    //         req.password = hash;
-    //         req.save(res);
-
-    //     });
-    // });
     find: function (req, res) {
         db.Users
             .findById({ _id: req.params.id })
@@ -24,20 +9,15 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
-        let qi;
         bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(req.body.password, salt, function (err, hash) {
-                qi = {
+                db.Users.create({
                     name: req.body.name,
                     email: req.body.email,
                     password: hash
-                }
+                }).then(dbModel => res.json(dbModel)).catch(err => res.status(422).json(err));
             });
         });
-        db.Users
-            .create(qi)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
     },
     remove: function (req, res) {
         db.Users
