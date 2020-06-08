d85729b29078e37562d560ca150f01b5cade70f9
John Ochs <john.w.ochs0@gmail.com>
2020-06-08 12:10:27 -0500

    about section now updates with the modification section

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./style.css";
+import api from "../../utils/api";
 
-function About() {
-    return (
-        <>
-            <div className="centaur">
-                <div className="intermodal">
-                    <p>about</p>
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            about: null
+        }
+    }
+
+    componentDidMount = () => api.findBlog("5ed6749f8efd8459d3cb82f5").then(res => this.setState({ about: res.data.content }));
+
+    render() {
+        return (
+            <>
+                <div className="centaur">
+                    <div className="intermodal">
+                        <div dangerouslySetInnerHTML={{ __html: this.state.about }} />
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default About;
