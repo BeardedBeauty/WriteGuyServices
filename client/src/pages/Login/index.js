0d48f2e67c272ced7ed093bcc0f3405136847d82
John Ochs <john.w.ochs0@gmail.com>
2020-06-15 16:50:24 -0500

    states dont work but the register page is technically done

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -17,8 +17,9 @@ class Login extends React.Component {
             invalid: "",
             redirect: false,
             loading: true,
-            DTdrawer: ["login loginOpenDT z-depth-3", "register registerClosedDT"],
-            drawerState: true
+            DTdrawer: ["loginOpenDT z-depth-3", "registerClosedDT"],
+            drawerState: true,
+            pleaseLogin: false
         }
     }
 
@@ -63,7 +64,13 @@ class Login extends React.Component {
         else if (this.state.registerpass !== this.state.registerpassconfirm) this.setState({ invalid: "invalid" });
         else {
             this.setState({ invalid: "valid" });
+            console.log(this.state.DTdrawer)
             if (j !== "" && l !== "" && qr !== "" && x === qr) {
+                this.setState({
+                    DTdrawer: ["loginOpenDT z-depth-3", "registerClosedDT"],
+                    drawerState: true,
+                    pleaseLogin: true
+                });
                 api.newUser({
                     name: j,
                     email: l,
@@ -75,9 +82,7 @@ class Login extends React.Component {
     }
 
     auth = async () => api.authUser().then(res => {
-        if (res) {
-            this.setState({ loading: false, redirect: true });
-        }
+        if (res) this.setState({ loading: false, redirect: true });
         else {
             const error = new Error(res.error);
             throw error;
@@ -106,42 +111,49 @@ class Login extends React.Component {
             <>
                 <div className="centaur">
                     <div className="intermodal">
+                        {this.state.pleaseLogin && <div className="pleaseLogin">Thank you for registering. Please log in to continue</div>}
                         <div className="logBlock">
-                            <div className={`login ${this.state.DTdrawer[0]}`} onClick={this.DTdrawers.bind(this, true)}>
-                                <h3>Log in</h3>
-                                <br /><br /><br />
-                                <form>
-                                    <label htmlFor="email_inline">Email</label>
-                                    <input id="email_inline" type="email" className="validate" onChange={y => this.setLoginEmail(y)} />
-                                    <span className="helper-text" data-error="Please enter a valid email" data-success="right"></span>
-                                    <br />
-                                    <label htmlFor="passw1">password</label>
-                                    <input type="password" id="passw1" name="passw1" onChange={a => this.setLoginPass(a)} />
-                                    <br /><br /><br /><br />
-                                    <button className="btn block green waves-effect waves-yellow" type="submit" name="action" onClick={d => {
-                                        this.loginSubmit(d);
-                                    }}>Log in</button>
-                                </form>
+                            <div className={`drawer login ${this.state.DTdrawer[0]}`} onClick={this.DTdrawers.bind(this, true)}>
+                                {!this.state.drawerState && <p>Log in</p>}
+                                {this.state.drawerState && <>
+                                    <h3>Log in</h3>
+                                    <br /><br /><br />
+                                    <form>
+                                        <label htmlFor="email_inline">Email</label>
+                                        <input id="email_inline" type="email" className="validate" onChange={y => this.setLoginEmail(y)} />
+                                        <span className="helper-text" data-error="Please enter a valid email" data-success="right"></span>
+                                        <br />
+                                        <label htmlFor="passw1">password</label>
+                                        <input type="password" id="passw1" name="passw1" onChange={a => this.setLoginPass(a)} />
+                                        <br /><br /><br /><br />
+                                        <button className="btn block green waves-effect waves-yellow" type="submit" name="action" onClick={d => {
+                                            this.loginSubmit(d);
+                                        }}>Log in</button>
+                                    </form>
+                                </>}
                             </div>
-                            <div className={`register ${this.state.DTdrawer[1]}`} onClick={this.DTdrawers.bind(this, false)}>
-                                <h3>r</h3>
-                                <form encType="multipart/form-data">
-                                    <label htmlFor="name">full name</label>
-                                    <input type="text" id="name" name="name" onChange={v => this.registerName(v)} />
-                                    <label htmlFor="email_inline2">Email</label>
-                                    <input id="email_inline2" type="email" className="validate" onChange={b => this.registerEmail(b)} />
-                                    <span className="helper-text" data-error="Please enter a valid email" data-success="Email valid"></span>
-                                    <br />
-                                    <label htmlFor="passw2">password</label>
-                                    <input type="password" id="passw2" name="passw2" className={this.state.invalid} onChange={n => this.registerPassword([n, false])} />
-                                    <label htmlFor="passwconfirm2">confirm password</label>
-                                    <input type="password" id="passwconfirm2" name="passwconfirm2" className={this.state.invalid} onChange={m => this.registerPassword([m, true])} />
-                                    <span className="helper-text" data-error="Passwords must match and minimum 8 characters in length" data-success=""></span>
-                                    <br /><br />
-                                    <button className={"btn block green waves-effect waves-light"} type="submit" name="action" onClick={qy => {
-                                        this.registerSubmit(qy);
-                                    }}>Submit<i className="material-icons right">send</i></button>
-                                </form>
+                            <div className={`drawer register ${this.state.DTdrawer[1]}`} onClick={this.DTdrawers.bind(this, false)}>
+                                {this.state.drawerState && <p>Register</p>}
+                                {!this.state.drawerState && <>
+                                    <h3>Register</h3>
+                                    <form encType="multipart/form-data">
+                                        <label htmlFor="name">full name</label>
+                                        <input type="text" id="name" name="name" onChange={v => this.registerName(v)} />
+                                        <label htmlFor="email_inline2">Email</label>
+                                        <input id="email_inline2" type="email" className="validate" onChange={b => this.registerEmail(b)} />
+                                        <span className="helper-text" data-error="Please enter a valid email" data-success="Email valid"></span>
+                                        <br />
+                                        <label htmlFor="passw2">password</label>
+                                        <input type="password" id="passw2" name="passw2" className={this.state.invalid} onChange={n => this.registerPassword([n, false])} />
+                                        <label htmlFor="passwconfirm2">confirm password</label>
+                                        <input type="password" id="passwconfirm2" name="passwconfirm2" className={this.state.invalid} onChange={m => this.registerPassword([m, true])} />
+                                        <span className="helper-text" data-error="Passwords must match and minimum 8 characters in length" data-success=""></span>
+                                        <br /><br />
+                                        <button className={"btn block green waves-effect waves-light"} type="submit" name="action" onClick={qy => {
+                                            this.registerSubmit(qy);
+                                        }}>Submit<i className="material-icons right">send</i></button>
+                                    </form>
+                                </>}
                             </div>
                         </div>
                         {/* <div id="signinDrawer" className={this.state.drawer[0] + "0 drawer"} onClick={openDrawer.bind(this, 0)}>
