83cd0b91db047c923d06195073849908d1acd331
John Ochs <john.w.ochs0@gmail.com>
2020-06-16 17:24:17 -0500

    blurred BG image for filling space

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import api from "../../utils/api";
 import "./style.css";
+import bless from "../../assets/papabless.JPG";
 
 function Home() {
     const [blog, change] = useState({});
     useEffect(() => { api.recentBlog().then(res => { if (res) change(res.data) }); }, []);
     return (
         <>
-            <div className="centaur">
+            <div className="showverflow left">
+                <img src={bless} alt="papabless" className="fillerimage" />
+            </div>
+            <div className="showverflow right">
+                <img src={bless} alt="papabless" className="fillerimage" />
+            </div>
+            <div className="centaur absolute">
                 <div className="intermodal">
-                    <p>home</p>
+                    <img src={bless} alt="papabless" className="papabless" />
                     <div className="recentBlog">
                         <img src={blog.image} alt="recentBlogImage" className="recentBlogImage" />
-                        <p>{blog.title}</p>
                         <Link to={`/blog/${blog._id}`}>
-                            <div className="readMore">read more</div>
+                            <div className="readMore">read</div>
                         </Link>
+                        <div id="blogTitle">
+                            <h5 id="title">{blog.title}</h5>
+                        </div>
                     </div>
                 </div>
             </div>
