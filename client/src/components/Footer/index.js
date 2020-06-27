92c0baf47ba058a553d8483be051ec5c025d5b5f
John Ochs <john.w.ochs0@gmail.com>
2020-06-26 22:37:35 -0500

    Cleaning up footer & accessories

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./style.css";
 import logo from "../../assets/wgs.png";
-// import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <>
             <footer class="page-footer teal darken-4">
                 <div class="container">
-                    <div class="row">
+                    <div class="row center">
                         <img src={logo} alt="Writeguyservices" className="logo" />
                     </div>
-                    <div className="row">
-                        <div class="col s12 m8 l8">
-                            <h5 class="white-text">Contact me</h5>
+                    <div className="row center">
+                        <div class="col s12">
+                            <h5 class="">Connect</h5>
                             <ul>
                                 <li><a href="mailto:casey@write-guyservices.net">Email</a></li>
                                 <li><a className="" href="https://www.linkedin.com/in/casey-richard-ochs-write-guyservices/">Linkedin</a></li>
@@ -22,7 +21,7 @@ function Footer() {
                     </div>
                 </div>
                 <div class="footer-copyright">
-                    <div class="container">
+                    <div class="container center">
                         <p>© 2020 Write Guy Services</p>
                     </div>
                 </div>
